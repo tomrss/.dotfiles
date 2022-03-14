2309e8c3faa27ab66232f24960f263b631810e91
tomrss <tommaso.rossi1@protonmail.com>
2022-03-14 22:22:06 +0100

    Use cape package

--- emacs-alternatives/without-use-package/.emacs.d/init.el
+++ emacs-alternatives/without-use-package/.emacs.d/init.el
@@ -196,6 +196,27 @@
 
 (setq tab-always-indent 'complete)
 
+;;; Completion at point functions
+
+(straight-use-package 'cape)
+
+(add-to-list 'completion-at-point-functions #'cape-file)
+(add-to-list 'completion-at-point-functions #'cape-dabbrev)
+
+;; TODO following commented code seems promising but is not tested
+;; ;; Silence the pcomplete capf, no errors or messages!
+;; (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-silent)
+
+;; ;; Ensure that pcomplete does not write to the buffer
+;; ;; and behaves as a pure `completion-at-point-function'.
+;; (advice-add 'pcomplete-completions-at-point :around #'cape-wrap-purify)
+;; (add-hook 'eshell-mode-hook
+;;           (lambda ()
+;;             (setq-local corfu-quit-at-boundary t
+;;                         corfu-quit-no-match t
+;;                         corfu-auto nil)
+;;             (corfu-mode)))
+
 ;;;; Editing
 
 ;; navigable undo/redo tree
