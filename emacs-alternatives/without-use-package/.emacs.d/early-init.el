06605cf6f365b76a81e977353b517d9bcb4eaf51
tomrss <tommaso.rossi1@protonmail.com>
2022-03-22 08:39:17 +0100

    Use emacs-startup-hook instead of after-init-hook

--- emacs-alternatives/without-use-package/.emacs.d/early-init.el
+++ emacs-alternatives/without-use-package/.emacs.d/early-init.el
@@ -15,20 +15,20 @@
 
 ;;;; Optimize garbage collections
 
-(defconst tr/gc-cons-standard-threshold-mb 1024
+(defconst +gc-cons-standard-threshold-mb 1024
   "Number of MB of consing between garbage collection during normal operativity.")
-(defconst tr/gc-cons-startup-threshold-mb 2048
+(defconst +gc-cons-startup-threshold-mb 2048
   "Number of MB of consing between garbage collection during startup.")
 
-(defun tr/restore-garbage-collection ()
-  "Restore GC consing threshold to `tr/gc-cons-standard-threshold-mb'."
-  (setq gc-cons-threshold (* tr/gc-cons-standard-threshold-mb 1024 1024)))
+(defun +restore-garbage-collection ()
+  "Restore GC consing threshold to `+gc-cons-standard-threshold-mb'."
+  (setq gc-cons-threshold (* +gc-cons-standard-threshold-mb 1024 1024)))
 
 ;; set high garbage collection consing threshold during startup
-(setq gc-cons-threshold (* tr/gc-cons-startup-threshold-mb 1024 1024))
+(setq gc-cons-threshold (* +gc-cons-startup-threshold-mb 1024 1024))
 
 ;; restore garbage collection after init
-(add-hook 'after-init-hook 'tr/restore-garbage-collection)
+(add-hook 'emacs-startup-hook '+restore-garbage-collection)
 
 ;; garbage collect when idle
 (run-with-idle-timer 2 t 'garbage-collect)
@@ -38,11 +38,11 @@
 (defvar default-file-name-handler-alist file-name-handler-alist)
 (setq file-name-handler-alist nil)
 
-(defun tr/reset-file-name-handler-alist ()
+(defun +reset-file-name-handler-alist ()
   "Reset filename handlers to default value."
   (setq file-name-handler-alist default-file-name-handler-alist))
 
-(add-hook 'after-init-hook 'tr/reset-file-name-handler-alist)
+(add-hook 'emacs-startup-hook '+reset-file-name-handler-alist)
 
 ;;;; Disable package.el at startup
 
