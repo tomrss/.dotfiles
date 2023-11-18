437cf073a041a1116abbf00c6f513265a42b49f2
tomrss <tommaso.rossi1@protonmail.com>
2023-11-18 20:38:24 +0100

    Set the path of emacs native compilation cache

--- home.scm
+++ home.scm
@@ -90,7 +90,7 @@
                           ("_JAVA_AWT_WM_NONREPARENTING" . "1")
                           ("QT_QPA_PLATFORM" . "wayland")
                           ("CALIBRE_USE_DARK_PALETTE" . "1")
-                          ))
+                          ("EMACSNATIVELOADPATH" . "/tmp:/home/tomrss/.cache/emacs/eln-cache/")))
         (service home-xdg-configuration-files-service-type
                  `(("sway" ,(local-file "sway" #:recursive? #t))
                    ("alacritty" ,(local-file "alacritty" #:recursive? #t))
