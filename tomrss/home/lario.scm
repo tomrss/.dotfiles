230526a001562a944a86ce09dcec55f4519d4b09
tomrss <tommaso.rossi1@protonmail.com>
2025-01-22 15:59:58 +0100

    Update media server cfg

--- tomrss/home/lario.scm
+++ tomrss/home/lario.scm
@@ -1,111 +1,54 @@
-;; TODO refactor with services
 (define-module (tomrss home lario)
+  #:use-module (tomrss packages jellyfin)
+  #:use-module (guix gexp)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages algebra)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages dns)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages lsof)
+  #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages wm)
+  #:use-module (gnu services)
+  #:use-module (gnu home services shells)
   #:use-module (gnu home)
   #:use-module (gnu home services)
-  #:use-module (gnu packages)
-  #:use-module (gnu packages shells)
-  #:use-module (gnu services)
-  #:use-module (guix gexp)
-  #:use-module (gnu home services shells))
+  #:use-module (gnu home services pm)
+  #:use-module (gnu home services desktop)
+  #:use-module (gnu home services syncthing))
 
 (home-environment
- (packages
-  (specifications->packages
-   '(
-     ;; editors and terminals
-     "alacritty"
-     "emacs-next-pgtk"
-
-     ;; desktop
-     "sway"
-     "waybar"
-     "fuzzel"
-     "adwaita-icon-theme"
-     "hicolor-icon-theme"
-     "pavucontrol"
-     "xdg-utils"
-     "gnome-commander"
-     "swaynotificationcenter"
-
-     ;; browsers
-     "firefox"
-     "qutebrowser"
-
-     ;; media
-     "qbittorrent"
-     "mkvtoolnix"
-     "mpv"
-     "qview"
-     "youtube-dl"
-     "calibre"
-
-     ;; fonts
-     "font-jetbrains-mono"
-
-     ;; networking
-     "openssh"
-     "openvpn"
-     "sshfs"
-     "protonvpn-cli"
-     "curl"
-     "nmap"
-     "netcat"
-     "net-tools"
-
-     ;; lib
-     "ncurses"
-     "libvterm"
-     "qtwayland"
-     "libnotify"
-
-     ;; util
-     "imagemagick"
-     "ghostscript"
-     "cifs-utils"
-     "tree"
-     "ripgrep"
-     "unzip"
-     "zip"
-     "unrar-free"
-     "lsof"
-     "bc"
-
-     ;; toolchain
-     "gcc-toolchain"
-     "cmake"
-     "make"
-     "pkg-config"
-     "ccls"
-     "bear"
-     "guile"
-     "python"
-     "python-pip"
-     "python-virtualenv"
-     "go"
-     "docker-compose"
-     )))
-
+ (packages 
+   ;; kiosk with jellyfin client
+   (list cage
+         jellyfin-media-player
+         ;; networking utils
+         curl
+         nmap
+         netcat
+         net-tools
+         lsof
+         openresolv
+         ;; other nice utilities
+         tree
+         ripgrep
+         zip
+         unzip
+         bc))
  (services
   (list (service home-bash-service-type
                  (home-bash-configuration
-                  (bashrc (list (local-file "../../.bashrc" "bashrc")))
-                  (bash-profile (list (local-file "../../.bash_profile" "bash_profile")))))
-        (simple-service 'home-env-var-service
-		                home-environment-variables-service-type
-		                `(
-                          ("_JAVA_AWT_WM_NONREPARENTING" . "1")
-                          ("QT_QPA_PLATFORM" . "xcb")
-                          ("QUTE_QT_WRAPPER" . "PyQt6")
-                          ("CALIBRE_USE_DARK_PALETTE" . "1")
-                          ("PATH" . "$PATH:$HOME/go/bin")))
-        (service home-xdg-configuration-files-service-type
-                 `(("sway" ,(local-file "../../sway" #:recursive? #t))
-                   ("alacritty" ,(local-file "../../alacritty" #:recursive? #t))
-                   ("waybar" ,(local-file "../../waybar" #:recursive? #t))
-                   ("fuzzel" ,(local-file "../../fuzzel" #:recursive? #t))
-                   ("swaync" ,(local-file "../../swaync" #:recursive? #t))
-                   ;; don't copy the whole folder because some files are to be written from qutebrowser
-                   ;; TODO: move that kind of files to another folder from qutebrowser config
-                   ("qutebrowser/themes" ,(local-file "../../qutebrowser/themes" #:recursive? #t))
-                   ("qutebrowser/config.py" ,(local-file "../../qutebrowser/config.py"))
-                   ("qutebrowser/quickmarks" ,(local-file "../../qutebrowser/quickmarks")))))))
+		  (bashrc       (list (local-file 
+					"../../.bashrc"
+					"bashrc")))
+                  (bash-profile (list (plain-file 
+					"start-jellyfin-kiosk"
+					(string-append
+					  "# If running from tty1 start jellyfin kiosk\n"
+					  "[ \"$(tty)\" = \"/dev/tty1\" ] && "
+					  "cage -d -- jellyfinmediaplayer --platform wayland"))))))
+	(service home-dbus-service-type))))
--- tomrss/systems/lario.scm
+++ tomrss/systems/lario.scm
@@ -1,29 +1,189 @@
-(define-module (tomrss systems lario)
+# TODO use base system
+(define-module (tomrss systems base)
   #:use-module (gnu)
-  #:use-module (guix)
-  #:use-module (tomrss systems base))
+  #:use-module (gnu services base)
+  #:use-module (gnu services avahi)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services docker)
+  #:use-module (gnu services ssh)
+  #:use-module (gnu services networking)
+  #:use-module (gnu services xorg)
+  #:use-module (gnu services pm)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages gnome)     ; libnotify
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages vim)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages wm)
+  #:use-module (guix packages)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 format)
+  #:use-module (ice-9 ftw)
+  #:use-module (ice-9 string-fun)
+  #:use-module (ice-9 textual-ports)
+  #:use-module (nongnu packages linux)
+  #:use-module (nongnu system linux-initrd))
+
+(define %user-uid 1000)
+
+(define (nonguix-key)
+  (plain-file "nonguix.pub"
+              "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+
+(define (auto-login-to-tty config tty user)
+  "Auto login on specific TTY.
+
+  From the Guix cookbook:
+  https://guix.gnu.org/cookbook/en/html_node/Auto_002dLogin-to-a-Specific-TTY.html
+  "
+  (if (string=? tty (mingetty-configuration-tty config))
+      (mingetty-configuration
+       (inherit config)
+       (auto-login user)
+	   (login-pause? #t))
+      config))
 
 (operating-system
-  (inherit base-system)
-  (host-name "lario")
+ ;;; Base system configuration
+ (host-name "lario")
+ (kernel linux)
+ (firmware (list linux-firmware))
+ (initrd microcode-initrd)
 
-  (bootloader
-   (bootloader-configuration
-    (bootloader grub-bootloader)
-    (targets (list "/dev/sda"))
-    (timeout 2)
-    (keyboard-layout (operating-system-keyboard-layout base-system))))
+ ;;; Localization
+ (locale "en_US.utf8")
+ (timezone "Europe/Rome")
+ (keyboard-layout (keyboard-layout "it" #:options '("caps:escape")))
 
-  (mapped-devices
-   (list (mapped-device
-          (source (uuid "e82bd059-09e7-4abf-b080-938790f4fcca"))
-          (target "root-partition")
-          (type luks-device-mapping))))
+ ;;; Users and groups
+ (users (cons*
+         (user-account
+          (name "tomrss")
+          (comment "Tommaso Rossi")
+          (uid %user-uid)
+          (group "users")
+          (home-directory "/home/tomrss")
+          (supplementary-groups '("wheel"
+                                  "netdev"
+                                  "docker"
+                                  "audio"
+                                  "tty"
+                                  "input"
+                                  "video")))
+         %base-user-accounts))
 
-  (file-systems
-   (cons* (file-system
-            (device (file-system-label "root-partition"))
-            (mount-point "/")
-            (type "ext4")
-            (dependencies mapped-devices))
-          %base-file-systems)))
+ ;;; Base packages for minimal system functionality
+ (packages (cons*
+            brightnessctl
+            pulseaudio
+            git
+            vim
+            gnu-make
+            stow
+            %base-packages))
+
+ ;;; Shepherd services
+ (services
+  (append
+   (list
+    ;;; Networking
+    ;;; https://guix.gnu.org/manual/devel/en/html_node/Networking-Setup.html
+    ;;; https://guix.gnu.org/manual/devel/en/html_node/Networking-Services.html
+
+    ;; needed by network manager
+    (service wpa-supplicant-service-type)
+    ;; network manager will take care of most of it. just add vpn plugin
+    (service network-manager-service-type
+             (network-manager-configuration
+              (vpn-plugins
+               (list network-manager-openvpn))))
+    ;; time sync
+    (service ntp-service-type)
+    ;; enable ssh server on port 2222
+    (service openssh-service-type
+             (openssh-configuration
+              (permit-root-login #f)
+              (openssh openssh-sans-x)
+              (port-number 2222)))
+    ;; cellular modems
+    (service modem-manager-service-type)
+    ;; discoverability on the local network
+    (service avahi-service-type)
+
+    ;;; Desktop services 
+    ;;; https://guix.gnu.org/manual/en/html_node/Desktop-Services.html
+
+    (service elogind-service-type)
+    (service udisks-service-type)
+    (service upower-service-type)
+    (service dbus-root-service-type)    
+    fontconfig-file-system-service
+
+    ;;; Power management services
+    ;;; https://guix.gnu.org/manual/en/html_node/Power-Management-Services.html
+
+    ;; power saving
+    (service tlp-service-type
+             (tlp-configuration
+              (cpu-boost-on-ac? #t)))
+    ;; control thermal state
+    (service thermald-service-type)
+
+      ;;; Miscellaneous services
+      ;;; https://guix.gnu.org/manual/devel/en/html_node/Miscellaneous-Services.html
+
+    ;; containerd is needed by docker
+    (service containerd-service-type)
+    (service docker-service-type))
+
+   ;;; Base services
+   ;; https://guix.gnu.org/manual/en/html_node/Base-Services.html
+
+   (modify-services %base-services
+                    ;; configure nonguix substitutes in the guix daemon
+                    (guix-service-type
+                     config => (guix-configuration
+                                (inherit config)
+                                (substitute-urls
+                                 (append (list "https://substitutes.nonguix.org")
+                                         %default-substitute-urls))
+                                (authorized-keys
+                                 (cons* (nonguix-key)
+                                        %default-authorized-guix-keys))))
+		            (mingetty-service-type 
+		             config => (auto-login-to-tty
+                                config "tty1" "tomrss")))))
+
+ ;;; Configure the bootloader
+ 
+ (bootloader
+  (bootloader-configuration
+   (bootloader grub-efi-bootloader)
+   (targets (list "/boot/efi"))
+   (timeout 1)
+   (keyboard-layout keyboard-layout)))
+
+ ;;; File systems (fstab)
+
+ (swap-devices
+  (list (swap-space
+         (target (uuid "f62d8022-c3a7-4582-9d08-de83b0bfdb87")))))
+ (file-systems
+  (cons* (file-system
+          (mount-point "/boot/efi")
+          (device (uuid "DD7A-B2D2" 'fat32))
+          (type "vfat"))
+         (file-system
+          (mount-point "/")
+          (device (uuid "21399c6b-c6d5-41d1-b2e9-a0a3709cca8e"))
+          (type "ext4"))
+         (file-system
+          (mount-point "/var/homemediaserver/data")
+          (device (uuid "0dd23756-888f-4fa7-b1c3-ba27ba336873"))
+          (type "ext4"))
+         %base-file-systems)))
