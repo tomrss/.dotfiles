ae998e002fa7c5619cd9fa76b1f7473bf4518317
tomrss <tommaso.rossi1@protonmail.com>
2025-05-24 14:00:18 +0200

    Update jellyfin package

--- tomrss/packages/jellyfin.scm
+++ tomrss/packages/jellyfin.scm
@@ -27,60 +27,60 @@
 
 (define-public jellyfin-media-player
   (package
-    (name "jellyfin-media-player")
-    (version "v1.11.1")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/jellyfin/jellyfin-media-player")
-             (commit "v1.11.1")))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "09c13qvdil76mlfc372jss9qk578dfrb0nyayh4a8l9kcj8zij96"))))
-    (inputs (list libtool
-                  harfbuzz
-                  freetype
-                  fontconfig
-                  libx11
-                  libxrandr
-                  libvdpau
-                  libva
-                  mesa-headers
-                  egl-wayland
-                  yasm
-                  alsa-lib
-                  pulseaudio
-                  uchardet
-                  zlib
-                  fribidi
-                  gnutls
-                  sdl2
-                  qtwebengine-5
-                  qtquickcontrols-5
-                  qtbase-5
-                  qtwayland-5
-                  qtx11extras
-                  curl
-                  wget
-                  unzip
-                  python
-                  mpv))
-    (native-inputs (list ninja python-wrapper))
-    (build-system qt-build-system)
-    (arguments
-     `(#:tests? #f
-       #:configure-flags (list "-GNinja")
-       #:phases (modify-phases %standard-phases
-                  (replace 'build
-                    (lambda _
-                      (invoke "ninja" "-j"
-                              (number->string (parallel-job-count)))))
-                  (replace 'install
-                    (lambda _
-                      (invoke "ninja" "install"))))))
-    (home-page "https://github.com/jellyfin/jellyfin-media-player")
-    (synopsis "Jellyfin Desktop Client")
-    (description "Desktop client for Jellyfin, the free software media system.
+   (name "jellyfin-media-player")
+   (version "v1.12.0")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://github.com/jellyfin/jellyfin-media-player")
+           (commit version)))
+     (file-name (git-file-name name version))
+     (sha256
+      (base32 "13709nbrn1xs7p4vxbxny7r8s7ynpbbl79z6dfz7axnsx74sfy11"))))
+   (inputs (list libtool
+                 harfbuzz
+                 freetype
+                 fontconfig
+                 libx11
+                 libxrandr
+                 libvdpau
+                 libva
+                 mesa-headers
+                 egl-wayland
+                 yasm
+                 alsa-lib
+                 pulseaudio
+                 uchardet
+                 zlib
+                 fribidi
+                 gnutls
+                 sdl2
+                 qtwebengine-5
+                 qtquickcontrols-5
+                 qtbase-5
+                 qtwayland-5
+                 qtx11extras
+                 curl
+                 wget
+                 unzip
+                 python
+                 mpv))
+   (native-inputs (list ninja python-wrapper))
+   (build-system qt-build-system)
+   (arguments
+    `(#:tests? #f
+      #:configure-flags (list "-GNinja")
+      #:phases (modify-phases %standard-phases
+                              (replace 'build
+                                       (lambda _
+                                         (invoke "ninja" "-j"
+                                                 (number->string (parallel-job-count)))))
+                              (replace 'install
+                                       (lambda _
+                                         (invoke "ninja" "install"))))))
+   (home-page "https://github.com/jellyfin/jellyfin-media-player")
+   (synopsis "Jellyfin Desktop Client")
+   (description "Desktop client for Jellyfin, the free software media system.
 It uses jellyfin-web with embedded MPV player.")
-    (license license:gpl2)))
+   (license license:gpl2)))
